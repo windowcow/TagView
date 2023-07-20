be03bdaa1e5f076564e804ec0c24f6b0ec4696fa
windowcow <strongwindowcow@gmail.com>
2023-07-20 16:53:21 +0900

    First commit

--- TagView/ContentView.swift
+++ TagView/ContentView.swift
@@ -10,6 +10,18 @@ import SwiftData
 
 struct ContentView: View {
     var body: some View {
-        TagView()
+        makeView {
+            MyView(text: "111")
+            MyView(text: "122")
+            MyView(text: "133")
+        }
+    }
+}
+
+struct MyView: View {
+    @State var text: String
+    var body: some View {
+        Text(text)
+
     }
 }
