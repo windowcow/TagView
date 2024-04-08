22035d8abd42cb93848844a932af8f58c89644e3
windowcow <strongwindowcow@gmail.com>
2024-04-08 12:46:52 +0900

    Initial Commit

---
+++ TagView/TagViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagViewApp.swift
+//  TagView
+//
+//  Created by windowcow on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TagViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
